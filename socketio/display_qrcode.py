0b1a28d9fdb083d9d91ceb55994ab34e423f9189
Gabriel Ziegler <gabrielziegler3@gmail.com>
2019-11-29 23:58:53 -0300

    Plot image with matplot
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

--- socketio/display_qrcode.py
+++ socketio/display_qrcode.py
@@ -1,12 +1,13 @@
 
 import qrcode
-import pygame
+# import pygame
 import netifaces as ni
-from pygame.locals import *
+import matplotlib.pyplot as plt
+# from pygame.locals import *
 
 
 def get_ipaddr():
-    network_interface = 'enp34s0'
+    network_interface = 'wlp3s0'
     ip = ni.ifaddresses(network_interface)[ni.AF_INET][0]['addr']
     return ip
 
@@ -17,11 +18,20 @@ def get_qrcode_image():
     return img
 
 
-def display_image(img):
-    pygame.init()
-    WIDTH = 256
-    HEIGHT = 256
-    windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 0)
+# def display_image(img):
+#     pygame.init()
+#     WIDTH = 256
+#     HEIGHT = 256
+#     windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 0)
+#     img = get_qrcode_image()
+#     windowSurface.blit(img, (0, 0))
+#     pygame.display.flip()
+
+
+def plot_image():
     img = get_qrcode_image()
-    windowSurface.blit(img, (0, 0))
-    pygame.display.flip()
+    plt.imshow(img)
+    plt.show()
+
+
+plot_image()
